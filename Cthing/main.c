381852d60fd7d5a9c741aceb3437baf5546df984
James Cool <jeffreysbrother@gmail.com>
2016-05-14 18:23:06 -0700

    Initial Commit

---
+++ Cthing/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Cthing
+//
+//  Created by James Cool on 5/14/16.
+//  Copyright © 2016 jeffreysbrother. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
